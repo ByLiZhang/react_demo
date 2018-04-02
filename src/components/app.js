3275f8da7aeb8de244c86e08713d8f57b8f1b804
Li Zhang <bylizhang@gmail.com>
2018-04-02 15:03:01 -0700

    finished styles images

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
 
-const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
-    </div>
-);
 
-export default App;
+
+import React, { Component } from 'react';
+// import '../assets/css/app.css';
+// import logo from '../assets/images/logo.svg';
+
+export default props => {
+    return (
+	    <div>
+	        <div className="app">
+	            <h1 className='awesome'>Styled by /dist css sheet</h1>
+	        </div>
+	    </div>
+    );
+};
+
+// export default App;
